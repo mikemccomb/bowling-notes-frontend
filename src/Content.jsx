e7662b084447dd5d2a6d3220e422dac6ea8d6c6c
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-10 15:06:55 -0500

    Sets Index Action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { SessionsIndex } from "./SessionsIndex";
+
 export function Content() {
+  // const sessions = [
+  //   { id: 1, date: "2023-04-07", gameone: 198, gametwo: 175, gamethree: 180, notes: "Does this work?" },
+  //   { id: 2, date: "2023-07-04", gameone: 198, gametwo: 175, gamethree: 180, notes: "Does this work?" },
+  // ];
+  const [sessions, setSessions] = useState([]);
+
+  const handleIndexSessions = () => {
+    console.log("handleIndexSessions");
+    axios.get("http://localhost:3000/league_sessions.json").then((response) => {
+      console.log(response.data);
+      setSessions(response.data);
+    });
+  };
+
+  useEffect(handleIndexSessions, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <SessionsIndex sessions={sessions} />
     </div>
   );
 }
---
+++ src/SessionsIndex.jsx
@@ -0,0 +1,17 @@
+export function SessionsIndex(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>All League Sessions</h1>
+      {props.sessions.map((session) => (
+        <div key={session.id}>
+          <h2>{session.date}</h2>
+          <p>Game One: {session.gameone}</p>
+          <p>Game Two: {session.gametwo}</p>
+          <p>Game Three: {session.gamethree}</p>
+          <p>Series: {session.series}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
