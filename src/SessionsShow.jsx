1aa9213c98461d7df1dcdd792fce752e63e14874
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-14 13:18:24 -0500

    Enables series update on game score changes

--- src/SessionsShow.jsx
+++ src/SessionsShow.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 /* eslint-disable react/prop-types */
 
 // Need to add handleSubmit; you can probably fix series in here?
 export function SessionsShow(props) {
+  const [games, setGames] = useState({
+    gameone: props.session.gameone,
+    gametwo: props.session.gametwo,
+    gamethree: props.session.gamethree,
+  });
+
+  const updateGame = (e) => {
+    const game = e.target.name;
+    setGames((existingValues) => ({ ...existingValues, [game]: +e.target.value }));
+  };
+  console.log(games);
+
   return (
     <div>
       <h1>Session Information</h1>
@@ -22,7 +35,7 @@ export function SessionsShow(props) {
             name="gameone"
             type="number"
             defaultValue={props.session.gameone}
-            // onChange={updateGame}
+            onChange={updateGame}
           />
         </div>
         <div>
@@ -32,7 +45,7 @@ export function SessionsShow(props) {
             name="gametwo"
             type="number"
             defaultValue={props.session.gametwo}
-            // onChange={updateGame}
+            onChange={updateGame}
           />
         </div>
         <div>
@@ -42,11 +55,11 @@ export function SessionsShow(props) {
             name="gamethree"
             type="number"
             defaultValue={props.session.gamethree}
-            // onChange={updateGame}
+            onChange={updateGame}
           />
         </div>
         <div>
-          Series: <input name="series" type="number" defaultValue={props.session.series} readOnly />
+          Series: <input name="series" type="number" value={games.gameone + games.gametwo + games.gamethree} readOnly />
         </div>
         <div>
           Notes: <input name="notes" type="text" defaultValue={props.session.notes} />
