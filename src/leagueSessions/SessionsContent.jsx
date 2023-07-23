e1e69073b3519549b558c2ab2a9d6dff36a3afba
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-23 12:24:05 -0500

    Rebuilds SessionsIndex

--- src/leagueSessions/SessionsContent.jsx
+++ src/leagueSessions/SessionsContent.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
 import { SessionsIndex } from "./SessionsIndex";
+import { useState, useEffect } from "react";
 
 export function SessionsContent() {
+  const [sessions, setSessions] = useState([]);
+
+  const handleIndexSessions = () => {
+    console.log("handleIndexSessions");
+    axios.get("http://localhost:3000/league_sessions.json").then((response) => {
+      console.log(response.data);
+      setSessions(response.data);
+    });
+  };
+
+  useEffect(handleIndexSessions, []);
+
   return (
     <div>
-      <SessionsIndex />
+      <SessionsIndex sessions={sessions} />
     </div>
   );
 }
--- src/leagueSessions/SessionsIndex.jsx
+++ src/leagueSessions/SessionsIndex.jsx
@@ -1,7 +1,31 @@
-export function SessionsIndex() {
+export function SessionsIndex(props) {
   return (
-    <div>
-      <h1>All Sessions</h1>
+    <div className="card-body">
+      <h3>League Sessions</h3>
+      <table className="table card-text">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Game 1</th>
+            <th>Game 2</th>
+            <th>Game 3</th>
+            <th>Series</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        {props.sessions.map((session) => (
+          <tbody key={session.id}>
+            <tr>
+              <th scope="row">{session.date}</th>
+              <td>{session.gameone}</td>
+              <td>{session.gametwo}</td>
+              <td>{session.gamethree}</td>
+              <td>{session.series}</td>
+              <td>{session.notes}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
