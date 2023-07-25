cab87663a182017753d51197758de8309a476d0c
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-25 17:07:42 -0500

    Fixes modal table

--- src/leagueSessions/SessionsEdit.jsx
+++ src/leagueSessions/SessionsEdit.jsx
@@ -36,72 +36,74 @@ export function SessionsEdit(props) {
       <h1>Session Information</h1>
       <form className="form-control" onSubmit={handleSubmit}>
         <table className="table">
-          <tr>
-            <td>
-              <label>Date:</label>
-            </td>
-            <td>
-              <input name="date" type="date" defaultValue={props.session.date} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label>Game 1:</label>
-            </td>
-            <td>
-              <input
-                maxLength="3"
-                name="gameone"
-                type="number"
-                defaultValue={props.session.gameone}
-                onChange={updateGame}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label>Game 2:</label>
-            </td>
-            <td>
-              <input
-                minLength="3"
-                name="gametwo"
-                type="number"
-                defaultValue={props.session.gametwo}
-                onChange={updateGame}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label>Game 3:</label>
-            </td>
-            <td>
-              <input
-                minLength="3"
-                name="gamethree"
-                type="number"
-                defaultValue={props.session.gamethree}
-                onChange={updateGame}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label>Series:</label>
-            </td>
-            <td>
-              <input name="series" type="number" value={games.gameone + games.gametwo + games.gamethree} readOnly />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label>Notes:</label>
-            </td>
-            <td>
-              <input name="notes" type="textarea" defaultValue={props.session.notes} />
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>
+                <label>Date:</label>
+              </td>
+              <td>
+                <input name="date" type="date" defaultValue={props.session.date} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Game 1:</label>
+              </td>
+              <td>
+                <input
+                  maxLength="3"
+                  name="gameone"
+                  type="number"
+                  defaultValue={props.session.gameone}
+                  onChange={updateGame}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Game 2:</label>
+              </td>
+              <td>
+                <input
+                  minLength="3"
+                  name="gametwo"
+                  type="number"
+                  defaultValue={props.session.gametwo}
+                  onChange={updateGame}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Game 3:</label>
+              </td>
+              <td>
+                <input
+                  minLength="3"
+                  name="gamethree"
+                  type="number"
+                  defaultValue={props.session.gamethree}
+                  onChange={updateGame}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Series:</label>
+              </td>
+              <td>
+                <input name="series" type="number" value={games.gameone + games.gametwo + games.gamethree} readOnly />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Notes:</label>
+              </td>
+              <td>
+                <input name="notes" type="textarea" defaultValue={props.session.notes} />
+              </td>
+            </tr>
+          </tbody>
         </table>
         <div className="container-fluid">
           <button className="btn btn-success" type="submit">
