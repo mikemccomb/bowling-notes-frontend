02fb70674caccedc5bd2ef0d573dc6eb802f4829
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-31 15:34:25 -0500

    Styles modal

--- src/About.jsx
+++ src/About.jsx
@@ -1,18 +1,20 @@
 export function About() {
   return (
-    <div>
-      <h1>About 39 Boards</h1>
-      <h2>Version 0.1</h2>
-      <ul>
-        <li>Enables Seasons CRUD</li>
-        <li>Enables League Sessions CRUD</li>
-      </ul>
-      <h2>Up Next</h2>
-      <ul>
-        <li>Bug fixes</li>
-        <li>League layer</li>
-      </ul>
-      <p>Developed by <a href="https://www.mikemccomb.net/" target="_blank" rel="noreferrer">Mike McComb</a></p>
+    <div className="card mt-3">
+      <h2 className="card-header">About 39 Boards</h2>
+      <div className="card-body">
+      <h3 className="card-subtitle mb-2 text-muted">Version 0.1</h3>
+        <ul>
+          <li>Enables Seasons CRUD</li>
+          <li>Enables League Sessions CRUD</li>
+        </ul>
+        <h2>Up Next</h2>
+        <ul>
+          <li>Bug fixes</li>
+          <li>League layer</li>
+        </ul>
+        <p>Developed by <a href="https://www.mikemccomb.net/" target="_blank" rel="noreferrer">Mike McComb</a></p>
+      </div>
     </div>
   )
   
