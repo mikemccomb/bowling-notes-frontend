5119584755bbbfd63c20e8ad0164cd5a23d198d2
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-31 15:09:58 -0500

    Creates About modal

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+export function About() {
+  return (
+    <div>
+      <h1>About 39 Boards</h1>
+      <h2>Version 0.1</h2>
+      <ul>
+        <li>Enables Seasons CRUD</li>
+        <li>Enables League Sessions CRUD</li>
+      </ul>
+      <h2>Up Next</h2>
+      <ul>
+        <li>Bug fixes</li>
+        <li>League layer</li>
+      </ul>
+      <p>Developed by <a href="https://www.mikemccomb.net/" target="_blank" rel="noreferrer">Mike McComb</a></p>
+    </div>
+  )
+  
+}
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import { About } from "./About";
+import {Modal} from "./Modal"
+
 export function Footer() {
+
+  const [showAbout, setShowAbout] = useState(false);
+
+  const handleShowAbout = () => {
+    setShowAbout(true);
+  }
+
+  const handleClose = () => {
+    setShowAbout(false);
+  }
+
+
   return (
     <footer className="container-fluid mt-2">
       <p>Copyright 2023</p>
+      <button onClick={handleShowAbout}>About</button>
+      <Modal show={showAbout} onClose={handleClose}>
+        <About />
+      </Modal>
     </footer>
   );
 }
